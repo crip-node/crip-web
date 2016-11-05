1b55005845774876b5f6d3b46aaeef361490ac65
Igors Krasjukovs <tahq69@github.com>
2016-11-05 11:57:13 +0200

    addDefault method now works with arrays;

--- crip/Methods.js
+++ crip/Methods.js
@@ -2,10 +2,14 @@ var crip = require('crip-core');
 var events = require('events');
 var utils = require('./Utils');
 
+
 /**
  * Creates new instance of crip public method holder
  * 
+ * @param {Object} gulp
  * @param {Config} config
+ * @param {Object} task
+ * @param {CripWeb} cripweb
  */
 function CripMethods(gulp, config, task, cripweb) {
     var self = this;
@@ -42,11 +46,16 @@ function CripMethods(gulp, config, task, cripweb) {
     /**
      * Public method to include gulp tasks
      * 
-     * @param {any} taskName Name of the task to include in crip default command
+     * @param {String|Array} task Task Name/Array of names to include in crip default command
      * @return {CripMethods}
      */
-    this.addDefault = function (taskName) {
-        cripweb._beforeDefault.push(taskName);
+    this.addDefault = function (task) {
+        if (crip.isArray(task))
+            crip.forEach(task, function (val) {
+                cripweb._beforeDefault.push(val);
+            });
+        else
+            cripweb._beforeDefault.push(task);
 
         return this;
     };
--- tests/crip/methods-tests.js
+++ tests/crip/methods-tests.js
@@ -11,33 +11,62 @@ describe('Methods', function () {
         expect(methods).to.have.property('config', 'config instance');
     });
 
-    it('define() should set property with object fn value', function () {
-        var methods = new Methods('config instance');
-        
-        function TaskConstructor() { }
-        TaskConstructor.prototype.fn = 'object-value';
+    describe('#define', function () {
 
-        methods.define('property-name', TaskConstructor);
+        it('should set property with object fn value', function () {
+            var methods = new Methods('config instance');
 
-        expect(methods).to.have.property('property-name', 'object-value');
+            function TaskConstructor() { }
+            TaskConstructor.prototype.fn = 'object-value';
+
+            methods.define('property-name', TaskConstructor);
+
+            expect(methods).to.have.property('property-name', 'object-value');
+        });
+
+        it('should rise error if method already defined', function () {
+            var methods = new Methods('config instance');
+            var delegate = function () {
+                methods.define('config');
+            };
+
+            expect(delegate).to.throw(Error);
+        });
+
+        it('should rise error if method definition is not set', function () {
+            var methods = new Methods('config instance');
+            function TaskConstructor() { }
+            var delegate = function () {
+                methods.define('test', TaskConstructor);
+            };
+
+            expect(delegate).to.throw(Error);
+        });
+
     });
 
-    it('define() should rise error if method already defined', function () {
-        var methods = new Methods('config instance');
-        var delegate = function () {
-            methods.define('config');
-        };
+    describe('#addDefault', function () {
 
-        expect(delegate).to.throw(Error);
-    });
+        it('should add string in to _beforeDefault array', function () {
+            var cripWeb = { _beforeDefault: new Array() };
+            var methods = new Methods('gulp', 'config', 'task', cripWeb);
 
-    it('define() should rise error if method definition is not set', function () {
-        var methods = new Methods('config instance');
-        function TaskConstructor() { }
-        var delegate = function () {
-            methods.define('test', TaskConstructor);
-        };
+            methods.addDefault('task-name');
 
-        expect(delegate).to.throw(Error);
+            expect(cripWeb._beforeDefault.length).to.equal(1);
+            expect(cripWeb._beforeDefault[0]).to.equal('task-name');
+        });
+
+        it('should join arrays', function () {
+            var cripWeb = { _beforeDefault: new Array('existing') };
+            var methods = new Methods('gulp', 'config', 'task', cripWeb);
+
+            methods.addDefault('task-name');
+
+            expect(cripWeb._beforeDefault.length).to.equal(2);
+            expect(cripWeb._beforeDefault[0]).to.equal('existing');
+            expect(cripWeb._beforeDefault[1]).to.equal('task-name');
+        });
+
     });
 });
